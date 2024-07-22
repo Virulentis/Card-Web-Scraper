b19e9b1be477c4ca5c11634506982bdbb38d191f
Virulentis <61526309+Virulentis@users.noreply.github.com>
2024-07-21 19:01:03 -0700

    Created a logger, refactored code to be more readable.

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import re
 import pandas as pd
-import F2F_scraper
 import config
-import wizards_tower_scraper
-import scraper_401
+import web_interaction
+import logging
+import sys
 
 
 def text_to_list() -> list[str]:
+    """
+    Converts a .txt file into a list of card names.
+    Expects conventional decklist notation `#   CardName`
+    :return: a list of card names
+    """
+
     file_object = open(config.FILENAME, "r")
     lines = file_object.readlines()
     key_list = [re.sub(r'^\d+\s+', '', item).rstrip('\n') for item in lines]
@@ -15,31 +21,44 @@ def text_to_list() -> list[str]:
     return key_list
 
 
-keyword_list = text_to_list()
-master_card_list = []
-f2f_card_list = []
-wiz_card_list = []
-g401_card_list = []
-
-if config.IS_F2F_SCRAPE:
-    print("Scraping F2F")
-    f2f_card_list = F2F_scraper.scrape_f2f(keyword_list)
-
-if config.IS_WIZ_SCRAPE:
-    print("Scraping WIZ")
-    wiz_card_list = wizards_tower_scraper.scrape_wizards(keyword_list)
-
-if config.IS_401_SCRAPE:
-    print("Scraping 401")
-    g401_card_list = scraper_401.scrape_401(keyword_list)
+def instantiate_logger():
+    """
+    creates a logger and sets the format and color.
+    :return: logger
+    """
+    green = '\033[01;32m'
+    color = green
+    logging.basicConfig(format=color + '%(asctime)s %(message)s', level=logging.INFO)
+    logger = logging.getLogger("Card_Logger")
+    return logger
 
 
-master_card_list = f2f_card_list + wiz_card_list + g401_card_list
+def main():
+    keyword_list = text_to_list()
+    master_card_list = []
+    f2f_card_list = []
+    wiz_card_list = []
+    g401_card_list = []
+    logger = instantiate_logger()
+
+    if config.IS_F2F_SCRAPE:
+        f2f_card_list = web_interaction.find_retailer_pages(keyword_list, "F2F")
+
+    if config.IS_WIZ_SCRAPE:
+        wiz_card_list = web_interaction.find_retailer_pages(keyword_list, "WIZ")
+
+    if config.IS_401_SCRAPE:
+        g401_card_list = web_interaction.find_retailer_pages(keyword_list, "G401")
+
+    master_card_list += f2f_card_list + wiz_card_list + g401_card_list
+
+    df = pd.DataFrame(master_card_list)
+    logger.debug(df)
+    logger.info("Sorting dictionary")
+    df = df.sort_values(by=['card_name', 'price', 'is_foil', 'condition'])
+    df.to_csv(config.OUTPUT_PATH)
+    logger.info("Program finished.")
 
 
-df = pd.DataFrame(master_card_list)
-print(df)
-print("Sorting dictionary")
-df = df.sort_values(by=['card_name', 'price', 'is_foil', 'condition'])
-df.to_csv(config.OUTPUT_PATH)
-print("Program finished.")
+if __name__ == "__main__":
+    main()
