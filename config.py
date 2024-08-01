e22de511a03d30a72e96a8d0b3d565e3bf755e5a
Virulentis <61526309+Virulentis@users.noreply.github.com>
2024-08-01 14:51:18 -0700

    Added frame to card TypeDict. Changed price to a decimal.

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
-ALLOW_FOIL = False
-ALLOW_OUT_OF_STOCK = False
 FILENAME = r"test.txt"
 OUTPUT_PATH = r"results.csv"
+
+
+ALLOW_FOIL = True
+ALLOW_OUT_OF_STOCK = False
 IS_F2F_SCRAPE = True
 IS_WIZ_SCRAPE = True
 IS_401_SCRAPE = True
+
 
