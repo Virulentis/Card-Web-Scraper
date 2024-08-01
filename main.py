f3b094d613a8bc036d30d9571f50c3113962e095
Virulentis <61526309+Virulentis@users.noreply.github.com>
2024-07-31 18:43:06 -0700

    reformated main. moved the extra functions to utils.

--- main.py
+++ main.py
@@ -1,45 +1,16 @@
-import re
 import pandas as pd
 import config
+import utils
 import web_interaction
-import logging
-import sys
-
-
-def text_to_list() -> list[str]:
-    """
-    Converts a .txt file into a list of card names.
-    Expects conventional decklist notation `#   CardName`
-    :return: a list of card names
-    """
-
-    file_object = open(config.FILENAME, "r")
-    lines = file_object.readlines()
-    key_list = [re.sub(r'^\d+\s+', '', item).rstrip('\n') for item in lines]
-
-    file_object.close()
-    return key_list
-
-
-def instantiate_logger():
-    """
-    creates a logger and sets the format and color.
-    :return: logger
-    """
-    green = '\033[01;32m'
-    color = green
-    logging.basicConfig(format=color + '%(asctime)s %(message)s', level=logging.INFO)
-    logger = logging.getLogger("Card_Logger")
-    return logger
 
 
 def main():
-    keyword_list = text_to_list()
+    keyword_list = utils.text_to_list()
     master_card_list = []
     f2f_card_list = []
     wiz_card_list = []
     g401_card_list = []
-    logger = instantiate_logger()
+    logger = utils.instantiate_logger()
 
     if config.IS_F2F_SCRAPE:
         f2f_card_list = web_interaction.find_retailer_pages(keyword_list, "F2F")
@@ -55,6 +26,7 @@ def main():
     df = pd.DataFrame(master_card_list)
     logger.debug(df)
     logger.info("Sorting dictionary")
+    utils.cost_of_deck(df)
     df = df.sort_values(by=['card_name', 'price', 'is_foil', 'condition'])
     df.to_csv(config.OUTPUT_PATH)
     logger.info("Program finished.")
