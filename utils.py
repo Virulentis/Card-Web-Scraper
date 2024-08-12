0d61b1cd261b4d339be23d7133d6becc5f676c9d
Virulentis <61526309+Virulentis@users.noreply.github.com>
2024-08-11 23:02:33 -0700

    Added run_search.

--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
+import pandas as pd
 from pandas import DataFrame
 import logging
 import config
 import re
+
+import web_interaction
 
 
 def instantiate_logger() -> logging:
@@ -72,3 +75,34 @@ def find_card_frame(full_card_name: str) -> str:
             else:
                 res += f", {keyword}"
     return res
+
+
+def run_search():
+    keyword_list = text_to_list()
+    master_card_list = []
+    f2f_card_list = []
+    wiz_card_list = []
+    g401_card_list = []
+    logger = logging.getLogger("Card_Logger")
+
+    if config.IS_F2F_SCRAPE:
+        f2f_card_list = web_interaction.find_retailer_pages(keyword_list, "F2F")
+
+    if config.IS_WIZ_SCRAPE:
+        wiz_card_list = web_interaction.find_retailer_pages(keyword_list, "WIZ")
+
+    if config.IS_401_SCRAPE:
+        g401_card_list = web_interaction.find_retailer_pages(keyword_list, "G401")
+
+    master_card_list += f2f_card_list + wiz_card_list + g401_card_list
+
+    if master_card_list:
+        df = pd.DataFrame(master_card_list)
+        logger.debug(df)
+        logger.info("Sorting dictionary")
+        cost_of_deck(df)
+        df = df.sort_values(by=['card_name', 'price', 'is_foil', 'condition'])
+        df.to_csv(config.OUTPUT_PATH)
+    else:
+        logger.info("\033[31;1;4mFailed to find.\033[0m")
+    logger.info("\x1b[38;5;69mProgram finished.")
